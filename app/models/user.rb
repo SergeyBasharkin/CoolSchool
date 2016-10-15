11c662a963b2a187915462fda3d4573a19acf48b
RustamSafin <last.safin@yandex.ru>
2016-10-15 14:19:18 +0300

    vk column added

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   validates :full_name, presence: true
-  
+  validates_format_of :vk, :with => /^((http:\/\/|https:\/\/)?(www\.)?vk\.com\/(\w|\d)+?\/?)?.{0}$/, :multiline => true
 
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161014155027) do
+ActiveRecord::Schema.define(version: 20161015095746) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20161014155027) do
     t.datetime "updated_at",                                 null: false
     t.string   "role",                   default: "student", null: false
     t.string   "full_name"
+    t.string   "vk"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
