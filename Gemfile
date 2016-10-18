d25d24052e04acc0ae71f55d96d37de2b4d4b8ec
SergeyBasharkin <rock-n-rolllll@bk.ru>
2016-10-18 17:29:27 +0300

    add active admin

--- Gemfile
+++ Gemfile
@@ -6,7 +6,6 @@ gem 'rails', '4.2.5.1'
 # Use postgresql as the database for Active Record
 gem 'pg', '~> 0.15'
 # Use SCSS for stylesheets
-gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
@@ -32,6 +31,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # gem 'bcrypt', '~> 3.1.7'
 gem "foundation-rails"
 gem "foundation-icons-sass-rails"
+gem 'activeadmin', '~> 1.0.0.pre4'
+
 # Use Unicorn as the app server
 # gem 'unicorn'
 # Use Capistrano for deployment
---
+++ app/admin/user.rb
@@ -0,0 +1,39 @@
+ActiveAdmin.register User do
+
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+  permit_params :email, :password, :password_confirmation, :role
+
+
+  index do
+    selectable_column
+    id_column
+    column :email
+    column :current_sign_in_at
+    column :sign_in_count
+    column :created_at
+    column :role
+    actions
+  end
+
+  form do |f|
+    f.inputs "Admin Details" do
+      f.input :email
+      f.input :password
+      f.input :password_confirmation
+      f.input :role
+    end
+    f.actions
+  end
+
+end
