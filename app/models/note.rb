67e831730d599c09b1727b97a39513dfdaa81529
SergeyBasharkin <rock-n-rolllll@bk.ru>
2016-11-05 22:58:21 +0300

    fix slider

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,6 +1,6 @@
 class Note < ActiveRecord::Base
   validates :name,:number,:vk, presence: true
   validates_format_of :number, :with => /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/, :multiline => true
-  validates_format_of :vk, :with => /^(http:\/\/|https:\/\/)?(www\.)?vk\.com\/(\w|\d)+?\/?$/, :multiline => true
+  validates_format_of :vk, :with => /^(http:\/\/|https:\/\/)?(www\.)?vk\.com\/(\w|\d)+?\/?$/, :multiline => true, presence: false
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   validates :full_name, presence: true
-  validates_format_of :vk, :with => /^((http:\/\/|https:\/\/)?(www\.)?vk\.com\/(\w|\d)+?\/?)?.{0}$/, :multiline => true
-  mount_uploader :avatar, AvatarUploader
+  validates_format_of :vk, :with => /^((http:\/\/|https:\/\/)?(www\.)?vk\.com\/(\w|\d)+?\/?)?.{0}$/, :multiline => true, :presence => false
 
   has_many :stud_lessons, :class_name => "Lesson", :foreign_key => :student_id
   has_many :teach_lessons, :class_name => "Lesson", :foreign_key => :teacher_id
