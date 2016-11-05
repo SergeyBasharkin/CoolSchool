56f1102e050f6325c698741898037fcd897d0212
SergeyBasharkin <rock-n-rolllll@bk.ru>
2016-11-05 04:39:41 +0300

    add seed

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -20,9 +20,19 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # PUT /resource
-  # def update
-  #   super
-  # end
+  def update
+   @user = User.find(params[:id])
+   if params[:user][:password].blank?
+     @user.update_without_password(params[:user])
+   else
+     @user.update_attributes(params[:user])
+   end
+   if @user.errors.blank?
+     redirect_to admin_users_path, :notice => "User updated successfully."
+   else
+     render :edit
+   end
+ end
 
   # DELETE /resource
   # def destroy
